b599a52b361c9dc5f31c3fb4b4c582a92e0dbe55
fireball2489@gmail.com <fireball2489@gmail.com>
2024-03-05 12:28:37 -0500

    Finished Rock Paper scissors game

---
+++ javascript.js
@@ -0,0 +1,87 @@
+function getComputerChoice()
+{
+    let randomNum = Math.floor(Math.random() * 3);
+    switch (randomNum) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+        default:
+            return "ERROR";
+    }
+}
+
+function playRound(playerSelection, computerSelection)
+{
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection)
+    {
+        case "rock":
+            if (computerSelection == "rock")
+            {
+                return "Draw";
+            }
+            else if (computerSelection == "paper")
+            {
+                return "You Lose! Paper beats Rock"
+            }
+            else if (computerSelection == "scissors")
+            {
+                return "You Win! Rock beats Scissors"
+            }
+        
+        case "paper":
+            if (computerSelection == "rock")
+            {
+                return "You Win! Paper beats Rock"
+                break;
+            }
+            else if (computerSelection == "paper")
+            {
+                return "Draw"
+                break;
+            }
+            else if (computerSelection == "scissors")
+            {
+                return "You Lose! Scissors beat Paper"
+                break;
+            }
+
+        case "scissors":
+            if (computerSelection == "rock")
+            {
+                return "You Lose! Rock beats Scissors"
+                break;
+            }
+            else if (computerSelection == "paper")
+            {
+                return "You Win! Scissors beat Paper"
+                break;
+            }
+            else if (computerSelection == "scissors")
+            {
+                return "Draw"
+                break;
+            }
+
+            
+    }
+
+
+}
+
+function playGame()
+{
+    for (let i = 0; i < 5; i++)
+    {
+        let playerSelection = prompt("Please enter rock paper or scissors")
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+}
