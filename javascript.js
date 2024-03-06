05e8274237e1a4d8be9cc730010b1802ff94cbf2
Jumony <fireball2489@gmail.com>
2024-03-06 15:11:37 -0500

    Finished up program. Ends when 5 rounds have occurred

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+let playerScore = 0;
+let computerScore = 0;
+let rounds = 0;
+const scoreTracker = document.createElement('span');
+document.body.appendChild(scoreTracker);
+
 function getComputerChoice()
 {
     let randomNum = Math.floor(Math.random() * 3);
@@ -18,6 +24,8 @@ function getComputerChoice()
 
 function playRound(playerSelection, computerSelection)
 {
+
+    rounds++;
     playerSelection = playerSelection.toLowerCase();
     switch (playerSelection)
     {
@@ -28,16 +36,21 @@ function playRound(playerSelection, computerSelection)
             }
             else if (computerSelection == "paper")
             {
+                computerScore++;
                 return "You Lose! Paper beats Rock"
             }
             else if (computerSelection == "scissors")
             {
+                playerScore++;
+                scoreTracker.textContent = playerScore;
                 return "You Win! Rock beats Scissors"
             }
         
         case "paper":
             if (computerSelection == "rock")
             {
+                playerScore++;
+                scoreTracker.textContent = playerScore;
                 return "You Win! Paper beats Rock"
                 break;
             }
@@ -48,6 +61,7 @@ function playRound(playerSelection, computerSelection)
             }
             else if (computerSelection == "scissors")
             {
+                computerScore++;
                 return "You Lose! Scissors beat Paper"
                 break;
             }
@@ -55,11 +69,14 @@ function playRound(playerSelection, computerSelection)
         case "scissors":
             if (computerSelection == "rock")
             {
+                computerScore++;
                 return "You Lose! Rock beats Scissors"
                 break;
             }
             else if (computerSelection == "paper")
             {
+                playerScore++;
+                scoreTracker.textContent = playerScore;
                 return "You Win! Scissors beat Paper"
                 break;
             }
@@ -69,19 +86,132 @@ function playRound(playerSelection, computerSelection)
                 break;
             }       
     }
+
+
 }
 
 
 
-const rockButton = document.querySelector('.rock-button')
+
+
+const rockButton = document.querySelector('.rock-button');
+const paperButton = document.querySelector('.paper-button');
+const scissorsButton = document.querySelector('.scissors-button')
+
+
 rockButton.addEventListener('click', () => { 
-    let computerSelection = getComputerChoice();
-    playRound('rock', computerSelection);
+    if (rounds === 4 && playerScore > computerScore)
+    {
+        const winnerText = document.createElement('p');
+        winnerText.textContent = "You Win!"
+        rounds = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.body.appendChild(winnerText);
+    }
+    else if (rounds === 4 && playerScore < computerScore)
+    {
+        const winnerText = document.createElement('p');
+        winnerText.textContent = "You Lose!";
+        rounds = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.body.appendChild(winnerText);
+    }
+    else if (rounds === 4 && playerScore === computerScore)
+    {
+        const winnerText = document.createElement('p');
+        winnerText.textContent = "You both have the same score!";
+        rounds = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.body.append(winnerText);
+    }
+    else
+    {
+        const container = document.createElement('div');
+        let computerSelection = getComputerChoice();
+        container.textContent = playRound('rock', computerSelection);
+        document.body.appendChild(container);
+        
+    }
+
 });
 
-function playGame()
-{
-    let playerSelection = prompt("Please enter rock paper or scissors")
-    let computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-}
+paperButton.addEventListener('click', () => {
+    if (rounds === 4 && playerScore > computerScore)
+    {
+        const winnerText = document.createElement('p');
+        winnerText.textContent = "You Win!"
+        rounds = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.body.appendChild(winnerText);
+    }
+    else if (rounds === 4 && playerScore < computerScore)
+    {
+        const winnerText = document.createElement('p');
+        winnerText.textContent = "You Lose!";
+        rounds = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.body.appendChild(winnerText);
+    }
+    else if (rounds === 4 && playerScore === computerScore)
+    {
+        const winnerText = document.createElement('p');
+        winnerText.textContent = "You both have the same score!";
+        rounds = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.body.append(winnerText);
+    }
+    else
+    {
+        const container = document.createElement('div');
+        let computerSelection = getComputerChoice();
+        container.textContent = playRound('paper', computerSelection);
+        document.body.appendChild(container);
+    }
+
+})
+
+scissorsButton.addEventListener('click', () => {
+    if (rounds === 4 && playerScore > computerScore)
+    {
+        const winnerText = document.createElement('p');
+        winnerText.textContent = "You Win!"
+        rounds = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.body.appendChild(winnerText);
+    }
+    else if (rounds === 4 && playerScore < computerScore)
+    {
+        const winnerText = document.createElement('p');
+        winnerText.textContent = "You Lose!";
+        rounds = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.body.appendChild(winnerText);
+    }
+    else if (rounds === 4 && playerScore === computerScore)
+    {
+        const winnerText = document.createElement('p');
+        winnerText.textContent = "You both have the same score!";
+        rounds = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.body.append(winnerText);
+    }
+    else
+    {
+        const container = document.createElement('div');
+        let computerSelection = getComputerChoice();
+        container.textContent = playRound('scissors', computerSelection);
+        document.body.appendChild(container);
+    }
+
+})
+
+
